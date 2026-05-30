6d8446f7ce78768ffe532d4cd21fe6a90f3cd81e
mohneeshsuri22-star <mohneeshsuri22@gmail.com>
2026-05-30 11:22:11 +0400

    Delete Frontend directory

--- Frontend/client/src/pages/LiveAlertsWrapper_India.tsx
+++
@@ -1,6 +0,0 @@
-import Live_Alerts_India from "./Live_Alerts_India";
-
-export default function LiveAlertsWrapper_India() {
-  // Remove PremiumRouteGuard and just render the page
-  return <Live_Alerts_India />;
-}
