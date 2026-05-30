5b9fd42e78a5027f2857e8eb608bef116b071cce
mohneeshsuri22-star <mohneeshsuri22@gmail.com>
2026-05-30 11:22:22 +0400

    Delete Frontend_v2 directory

--- Frontend_v2/client/src/components/PageWrapper.tsx
+++
@@ -1,7 +0,0 @@
-export default function PageWrapper({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="pt-16 md:pt-16 min-h-screen">
-      {children}
-    </main>
-  );
-}
--- Frontend_v2/client/src/pages/LiveAlertsWrapper_US.tsx
+++
@@ -1,6 +0,0 @@
-import Live_Alerts_US from "./Live_Alerts_US";
-
-export default function LiveAlertsWrapper_US() {
-  // Remove PremiumRouteGuard and just render the page
-  return <Live_Alerts_US />;
-}
